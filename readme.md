b4d380d6f05f123991d77e1625f610960f6b7001
552277 <15090552277@163.com>
2018-06-28 18:26:51 +0800

    服务提供者

---
+++ readme.md
@@ -0,0 +1,6 @@
+
+启动服务
+
+java -jar service-provider-0.0.1-SNAPSHOT.jar --server.port=8081
+
+java -jar service-provider-0.0.1-SNAPSHOT.jar --server.port=8082
---
+++ src/test/java/com/wuji/hello/HelloApplicationTests.java
@@ -0,0 +1,10 @@
+package com.wuji.hello;
+
+/*@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+}*/
